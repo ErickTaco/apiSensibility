7d9570e6c7d925001cc1301c643dccd085f67214
Erick Taco <erick.taco@ceer.ec>
2024-10-14 12:45:50 -0500

    Inicializando el repositorio y subiendo cambios

--- src/controllers/geoserverController.js
+++ src/controllers/geoserverController.js
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import axios from "axios";
 import proj4 from "proj4";
 import PDFDocument from "pdfkit";
 import fs from "fs";
@@ -7,16 +7,6 @@ import path from "path"; // Importar 'path' para manejar rutas
 const wgs84 = "+proj=longlat +datum=WGS84 +no_defs";
 const utm32717 = "+proj=utm +zone=17 +south +datum=WGS84 +units=m +no_defs";
 const buffer = 50; // Buffer para bbox
-
-// Función para aplicar timeout a las solicitudes fetch
-const fetchWithTimeout = (url, options = {}, timeout = 30000) => {
-  return Promise.race([
-    fetch(url, options),
-    new Promise((_, reject) =>
-      setTimeout(() => reject(new Error("Request timed out")), timeout)
-    ),
-  ]);
-};
 
 export const getMap = async (req, res) => {
   // Obtenemos longitud y latitud de los parámetros de consulta
@@ -46,14 +36,15 @@ export const getMap = async (req, res) => {
 
   // URL de GeoServer
   const url = `http://45.136.18.138:8080/geoserver/Ceer/wms?service=WMS&version=1.1.0&request=GetFeatureInfo&layers=${layer}&query_layers=${layer}&bbox=${bbox}&width=706&height=768&srs=EPSG:32717&info_format=application/json&x=123&y=456`;
-  console.log(url);
+  console.log(`URL de solicitud: ${url}`); // Log de la URL
 
   try {
-    // Usar la función fetchWithTimeout con un timeout de 30 segundos (30000 ms)
-    const response = await fetchWithTimeout(url, {}, 30000);
-    if (!response.ok) throw new Error("Error en la consulta WMS");
+    // Usar axios con un timeout de 30 segundos (30000 ms)
+    const response = await axios.get(url, { timeout: 30000 });
+    console.log(`Estado de respuesta: ${response.status}`); // Log del estado de respuesta
 
-    const data = await response.json();
+    const data = response.data;
+    console.log("Datos recibidos de GeoServer:", data); // Log de datos recibidos
 
     // Verificar si se obtuvieron características
     if (data.features.length > 0) {
@@ -73,6 +64,7 @@ export const getMap = async (req, res) => {
         .json({ message: "No se encontraron características." });
     }
   } catch (error) {
+    console.error("Error en la solicitud a GeoServer:", error.message); // Log de error
     return res.status(500).json({
       error: "Error fetching map from GeoServer",
       details: error.message,
