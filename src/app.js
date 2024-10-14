3dd2beae3729774a3501e07d7c1b5d67c109ec9e
Erick Taco <erick.taco@ceer.ec>
2024-10-14 12:46:24 -0500

    Inicializando el repositorio y subiendo cambios

--- src/app.js
+++ src/app.js
@@ -13,5 +13,5 @@ app.use("/api/geoserver", geoserverRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  console.log(`Servidor corriendoooo en el puerto ${PORT}`);
 });
